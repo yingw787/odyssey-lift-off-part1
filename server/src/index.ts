4b80784557b6bd5978c3f6e5c046243921a8c111
Ying Wang <yingw787.personal@gmail.com>
2024-02-13 10:50:20 -0500

    add mocks and executable schemas to data.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
 import { typeDefs } from "./schema";
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs })
+    })
+  });
   const { url } = await startStandaloneServer(server);
 
   console.log(`
