1082286e872eb07b118c98cac8a3aa60fd9564f9
Ying Wang <yingw787.personal@gmail.com>
2024-02-13 10:37:44 -0500

    Update server/src/index.ts add method startApolloServer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
