801c07da826f299fd7b25d001cdcdade067db38b
Ying Wang <yingw787.personal@gmail.com>
2024-02-13 16:14:17 -0500

    Update tracks.tsx adding QueryResult handler for GraphQL.

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 import { Layout } from "../components";
 import { gql } from "../__generated__";
 import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 const TRACKS = gql(`
   query GetTracks {
@@ -28,15 +29,13 @@ const TRACKS = gql(`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return "Loading...";
-
-  if (error) return `Error! ${error.message}`;
-
   return (
     <Layout grid>
+    <QueryResult error={error} loading={loading} data={data}>
       {data?.tracksForHome?.map((track) => (
         <TrackCard key={track.id} track={track} />
       ))}
+    </QueryResult>
     </Layout>
   );
 };
